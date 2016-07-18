59229d08f6aa6b1fc2a4d3f0512925ba2a298e40
Alexandre Garcia Mayans <alexandre.garcia.mayans.ext@nokia.com>
2016-07-18 10:11:01 +0200

    Added zmq hello-world

---
+++ c/zmq/ping-pong/zmq_client.c
@@ -0,0 +1,27 @@
+#include <zmq.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define MAX_MESSAGE_LENGTH 10
+
+int main(int argc, char *argv[])
+{
+    printf("++ Connecting to ping server ...\n");
+    void *context = zmq_ctx_new();
+    void *requester = zmq_socket(context, ZMQ_REQ);
+    zmq_connect(requester, "tcp://localhost:5555");
+
+    char buffer[MAX_MESSAGE_LENGTH];
+    for (int requestIt = 0; requestIt < 10; requestIt++) {
+        printf("++ [%d] Sending ping ...\n", requestIt);
+        zmq_send(requester, "ping", 4, 0);
+        zmq_recv(requester, buffer, MAX_MESSAGE_LENGTH, 0);
+        printf("++ [%d] Received: '%s'\n", requestIt, buffer);
+    }
+
+    zmq_close(requester);
+    zmq_ctx_destroy(context);
+
+    return 0;
+}
+
---
+++ c/zmq/ping-pong/zmq_server.c
@@ -0,0 +1,31 @@
+#include <zmq.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MAX_MESSAGE_LENGTH 10
+
+int main(int argc, char *arv[])
+{
+    void *context = zmq_ctx_new();
+    void *responder = zmq_socket(context, ZMQ_REP);
+    int rc = zmq_bind(responder, "tcp://*:5555");
+    if (rc != 0) {
+        fprintf(stderr, "-- Could not bind responder to socket (error was: %s)\n", strerror(errno));
+    }
+
+    char buffer[MAX_MESSAGE_LENGTH];
+    while (1) {
+        memset(buffer, 0, MAX_MESSAGE_LENGTH);
+        zmq_recv(responder, buffer, MAX_MESSAGE_LENGTH, 0);
+        printf("++ Received buffer: '%s'\n", buffer);
+        sleep(1);
+        zmq_send(responder, "pong", 4, 0);
+    }
+
+    zmq_close(responder);
+    zmq_ctx_destroy(context);
+
+    return 0;
+}
+
