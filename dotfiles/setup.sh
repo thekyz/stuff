b4dfcf8c8ad50b5545c0ebd7a9d6a2811e9cf717
thekyz <thekyz@debian>
2016-07-26 05:25:16 -0400

    Added vim plugins and filetype plugins + small corrections

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -1,26 +1,27 @@
 #/!bin/bash
 
+# uncomment if necessary
+#export http_proxy=
+#export https_proxy=$http_proxy
+
 su -c "apt-get install sudo"
 export EDITOR=vi
 su -c "visudo"
 
 sudo apt-get remove vim-tiny
 sudo apt-get update
-sudo apt-get install vim git python vim-gnome python-pip curl
-
-# uncomment if necessary
-#export http_proxy=
-#export https_proxy=$http_proxy
+sudo apt-get install vim git python vim-gnome python-pip curl silversearcher-ag shellcheck
 
 cd ~
 git clone https://www.github.com/stuff
 
-for i in $(find ~/stuff/dotfiles -maxdepth 1 -name ".*" -type f -printf "%P \n"); do
-    if [ -e ~/$i ]; then
-        mv ~/$i ~/$i.bkp
+shopt -s globstar nullglob
+for dotfile in ~/stuff/dotfiles/.*; do
+    if [ -e ~/"$dotfile" ]; then
+        mv ~/"$dotfile" ~/"$dotfile".bkp
     fi
 
-    ln -s ~/stuff/dotfiles/$i ~/$i
+    ln -s ~/stuff/dotfiles/"$dotfile" ~/"$dotfile"
 done
 
 source ~/.bashrc
--- dotfiles/setup_vim.sh
+++ dotfiles/setup_vim.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors
+mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors ~/.vim/ftplugin/sh.vim
+
+# Install pathogen
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+# Color schemes
 cd ~/.vim/colors
 if [ ! -f ~/.vim/colors/molokai.vim ]; then
     wget https://raw.githubusercontent.com/flazz/vim-colorschemes/master/colors/molokai.vim
 fi
 
+# And lets plugin !
 cd ~/.vim/bundle
 git clone --depth=1 https://github.com/scrooloose/syntastic.git
-git clone --depth=1 https://github.com/vim-airline/vim-airline
+git clone --depth=1 https://github.com/vim-airline/vim-airline.git
+git clone --depth=1 https://github.com/tpope/vim-dispatch.git
+git clone --depth=1 https://github.com/wincent/ferret.git
+git clone --depth=1 https://github.com/tpope/vim-surround.git
+
+# Filetype plugins ...
+git clone --depth=1 https://github.com/thekyz/vim-ftplugins.git ~/.vim/ftplugin
 
