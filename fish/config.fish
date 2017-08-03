91d49280781eb010cad3d1095d46c65b60fe0622
thekyz <thekyz@gmail.com>
2017-08-03 17:36:55 +0200

    added chips plugin manager to fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,11 @@
 set -x http_proxy emea.nics.nxp.com:8080
 set -x https_proxy emea.nics.nxp.com:8080
 
+for file in ~/.config/fish/conf.d/*.fish
+    source $file
+end
+
+# chips
+if [ -e ~/.config/chips/build.fish ] ; . ~/.config/chips/build.fish ; end
+
+set -x TERM xterm-256color
