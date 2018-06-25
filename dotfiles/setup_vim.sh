0a9d1e01c3fa9d5dcc8eba34e0ef4347f735af1a
thekyz <thekyz@gmail.com>
2018-06-25 21:24:05 +0000

    fixed vim setup

--- dotfiles/setup_vim.sh
+++ dotfiles/setup_vim.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors ~/.vim/ftplugin
-
-# Install pathogen
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ~/.vim/pack/git-plugins/start ~/.vim/colors
 
 # Color schemes
 cd ~/.vim/colors || exit -1
@@ -12,19 +9,13 @@ if [ ! -f ~/.vim/colors/molokai.vim ]; then
 fi
 
 # And lets plugin !
-cd ~/.vim/bundle || exit -1
-git clone --depth=1 https://github.com/scrooloose/syntastic.git
+cd ~/.vim/pack/git-plugins/start || exit -1
 git clone --depth=1 https://github.com/vim-airline/vim-airline.git
-git clone --depth=1 https://github.com/tpope/vim-dispatch.git
-#git clone --depth=1 https://github.com/wincent/ferret.git
-#git clone --depth=1 https://github.com/tpope/vim-surround.git
 git clone --depth=1 https://github.com/scrooloose/nerdtree.git
 git clone --depth=1 https://github.com/scrooloose/nerdcommenter.git
 git clone --depth=1 https://github.com/ctrlpvim/ctrlp.vim.git
-git clone --depth=1 https://github.com/FelikZ/ctrlp-py-matcher.git 
 git clone --depth=1 https://github.com/terryma/vim-multiple-cursors.git
-git clone --depth=1 https://github.com/jtratner/vim-flavored-markdown.git
-git clone --depth=1 https://github.com/fatih/vim-go.git
+git clone --depth=1 https://github.com/w0rp/ale.git
 
 # Filetype plugins ...
 git clone --depth=1 https://github.com/thekyz/vim-ftplugins.git ~/.vim/ftplugin
